dc294c70fd18fc46cf8e51a0425babe8f35cf750
IvanKhudyakov <halex7@yandex.ru>
2022-10-19 13:04:22 +0300

    Initial Commit

---
+++ TrafficLight/TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightApp.swift
+//  TrafficLight
+//
+//  Created by Иван Худяков on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
